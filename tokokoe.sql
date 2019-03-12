8c1c992982745c4b3e265421314ddb05926964dd
hilmankurniawan <namlihruk@gmail.com>
2019-03-12 15:53:33 +0700

    upload 2

--- tokokoe.sql
+++ tokokoe.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Feb 2019 pada 15.46
--- Versi server: 10.1.35-MariaDB
--- Versi PHP: 7.2.9
+-- Generation Time: Mar 12, 2019 at 09:49 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `idlevel` varchar(15) NOT NULL,
+  `nmlevel` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -37,22 +48,77 @@ CREATE TABLE `pelanggan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pelanggan`
+-- Dumping data for table `pelanggan`
 --
 
 INSERT INTO `pelanggan` (`pelangganId`, `namaPelanggan`, `email`, `telpon`, `alamat`) VALUES
 ('P000000001', 'HilmanKur', 'namlihruk@gmail.com', '25646785878', 'jakarta'),
 ('P000000002', 'Hilman Kur', 'Hilmankur@gmail.com', '081344566721', 'Jakarta');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `produkId` varchar(15) NOT NULL,
+  `nmProduk` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `satuan` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `image` text NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`produkId`, `nmProduk`, `harga`, `satuan`, `qty`, `image`, `deskripsi`) VALUES
+('P000000001', 'Lidi', 1000, 'pcs', 3, '', 'Jangan digigit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL,
+  `nmuser` varchar(20) NOT NULL,
+  `divisi` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`idlevel`);
+
+--
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`pelangganId`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`produkId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
